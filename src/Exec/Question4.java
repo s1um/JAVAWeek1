3716559a0f19398760aea6fdb50334ad4010f271
김민주 <siuuuum55@gmail.com>
2025-09-24 15:09:46 +0900

    JAVAQuestion

--- src/Exec/Question4.java
+++ src/Exec/Question4.java
@@ -2,20 +2,34 @@ package Exec;
 
 import java.util.Scanner;
 
-public class Question4 {
+public class Question4{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자: ");
         int first = sc.nextInt();
-
         System.out.print("두 번째 숫자: ");
         int second = sc.nextInt();
-
         System.out.print("연산자(+, -, *, /): ");
-        String o = sc.next();
+        String operation = sc.next();
 
-        System.out.println("결과: " + first + " " + o + " " + second + "=");
-
+        switch (operation){
+            case "+":
+                System.out.println(first + second);
+                break;
+            case "-":
+                System.out.println(first - second);
+                break;
+            case "*":
+                System.out.println(first * second);
+                break;
+            case "/":
+                System.out.println(first / second);
+                break;
+            default:
+                System.out.println("올바른 연산자를 입력하세요.");
+                break;
+        }
+        sc.close();
     }
 }
--- src/Exec/Question6.java
+++ src/Exec/Question6.java
@@ -2,37 +2,18 @@ package Exec;
 
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class Question6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("년도를 입력하세요: ");
+        int year = sc.nextInt();
 
-        System.out.print("월을 입력하세요 (1-12): ");
-        int month = sc.nextInt();
-
-        switch (month) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Invalid input");
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " 는 윤년입니다.");
+        } else {
+            System.out.println(year + " 는 윤년이 아닙니다.");
         }
     }
 }
