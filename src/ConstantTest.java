e451c5f490667c738de90a3583c6af5adf0b0f3d
Min Ku Lee <2025014@daelim.ac.kr>
2025-11-26 17:35:58 +0900

    Update ConstantTest.java

--- src/ConstantTest.java
+++ src/ConstantTest.java
@@ -1,18 +1,4 @@
 public class ConstantTest {
     public static void main(String[] args) {
-//수정 ㅋㅋ
-        int temperature = 20;  // 변수 선언 및 초기화
-        int FREEZING_POINT = 0;  // 상수 선언 및 초기화
-
-        // 변수 값 변경
-        temperature = 25;  // 'temperature' 변수의 값을 25로 변경
-        System.out.println("Current temperature: " + temperature);
-
-        // 상수 값 변경 시도 (컴파일 오류 발생)
-        FREEZING_POINT = 10;  // 이 코드는 컴파일 오류를 발생시킵니다.
-
-        System.out.println("Freezing point of water: " + FREEZING_POINT);
-	// 테스트
-        // 가나다라
-    }
+}
 }
